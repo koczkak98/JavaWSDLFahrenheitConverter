0d47be6d3969e02a7da45a748dce55be46a9fca9
koczkak98 <69437695+koczkak98@users.noreply.github.com>
2020-10-25 16:06:03 +0100

    add welcome.html, def a Fahrenheit class and error handling in ConvertController

--- JavaWSDLFahrenheitConverter/src/main/java/com/example/fahrenheitconverter/JavaWSDLFahrenheitConverter/converter/ConvertController.java
+++ JavaWSDLFahrenheitConverter/src/main/java/com/example/fahrenheitconverter/JavaWSDLFahrenheitConverter/converter/ConvertController.java
@@ -1,27 +1,56 @@
 package com.example.fahrenheitconverter.JavaWSDLFahrenheitConverter.converter;
 
 import JavaWSDLFahrenheitConverter.forras.AFDTempConverterEndpointServiceSoapBinding;
+import com.example.fahrenheitconverter.JavaWSDLFahrenheitConverter.model.Fahrenheit;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ConvertController {
 
-    @GetMapping("/{celsius}")
-    public String getCelsiusToFahrenheit
-            (@PathVariable("celsius") double c) throws Exception
+    @GetMapping ("/")
+    public String welcome (Model model)
     {
-        double fahrenheitValue = 0;
+        /** Add F° */
+        model.addAttribute("fahrenheit", new Fahrenheit());
 
-        System.out.println(fahrenheitValue);
+        return "welcome.html";
+    }
 
-        AFDTempConverterEndpointServiceSoapBinding service = new AFDTempConverterEndpointServiceSoapBinding("http://www.learnwebservices.com/services/tempconverter");
-        Double res = service.CelsiusToFahrenheit(c);
+    @PostMapping("/")
+    public String getCelsiusToFahrenheit
+            (Fahrenheit fahrenheit, Model model) throws Exception {
 
-        fahrenheitValue = res;
+            try {
+                String fahrenheitValue = fahrenheit.getInput();
+
+                System.out.println(fahrenheitValue);
+
+                /** comma problem */
+                if (fahrenheitValue.contains(",")) {
+                    fahrenheitValue = fahrenheitValue.replace(",", ".");
+                }
+
+                AFDTempConverterEndpointServiceSoapBinding service = new AFDTempConverterEndpointServiceSoapBinding("http://www.learnwebservices.com/services/tempconverter");
+                Double res = service.CelsiusToFahrenheit(Double.parseDouble(fahrenheitValue));
+
+                model.addAttribute("result", fahrenheitValue + " F°");
+            }
+            catch (IllegalArgumentException e)
+            {
+                model.addAttribute("error", "Incorrect type!");
+            }
+            catch (Exception e)
+            {
+                model.addAttribute("error", "Incorrect type!");
+            }
 
 
-        return ("F°: " + fahrenheitValue);
+
+        return "welcome.html";
     }
 }
---
+++ JavaWSDLFahrenheitConverter/src/main/java/com/example/fahrenheitconverter/JavaWSDLFahrenheitConverter/model/Fahrenheit.java
@@ -0,0 +1,16 @@
+package com.example.fahrenheitconverter.JavaWSDLFahrenheitConverter.model;
+
+public class Fahrenheit {
+
+    private String input;
+
+    public String getInput() {
+
+
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+}
